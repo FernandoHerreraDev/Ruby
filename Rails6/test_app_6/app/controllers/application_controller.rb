60bd2181b51d7925913df5d28135f12792851835
Fernando Herrera <fernando.herrera@enroutesystems.com>
2022-03-07 07:06:21 -0600

    adding Rails 6 proyect

---
+++ Rails6/test_app_6/app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+   # def hello
+    #    render html: 'Hello world!' #En este caso es el metodo que se definio en el root pero es necesario un template
+                                    #Por eso mismo se pone esta linea de codigo para hacer el render de un html que estara entre comillas
+    #end
+end
---
+++ Rails6/test_app_6/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  #root 'application#hello' # En esta parte referenciamos el root, dependiendo de que controlador queramos solo pondremos el nombre
+                     # Rails lo detecta solito al parecer
+                     #Para especificar un metodo dentro del controller se le pone un # como esta puesto ahorita
+
+  root 'pages#home'
+end
