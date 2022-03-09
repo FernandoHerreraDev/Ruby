6cee322452bbc64244433e40c68c5cfd958e4e23
Fernando Herrera <fernando.herrera@enroutesystems.com>
2022-03-09 16:36:57 -0600

    Finishing alpha-blog first part

--- Rails6/test_app_6/config/routes.rb
+++ Rails6/test_app_6/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   #root 'application#hello' # En esta parte referenciamos el root, dependiendo de que controlador queramos solo pondremos el nombre
                      # Rails lo detecta solito al parecer
                      #Para especificar un metodo dentro del controller se le pone un # como esta puesto ahorita
